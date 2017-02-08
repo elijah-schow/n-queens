ba1cf9404492429bc5bdff3a3ee0711b64e86692
elijah-schow <elijah.schow@gmail.com>
2017-02-08 16:16:43 -0600

    BROKEN: implement `findRooksSolutions()`
    AssertionError: expected 64 to equal 2 [in SpecRunner.html]

--- src/solvers.js
+++ src/solvers.js
@@ -16,7 +16,9 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = []; //fixme;[]
+
+  
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -24,7 +26,34 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = f(0, new Board({"n": n})); //fixme
+
+  function f(move, board) {
+    var solutionCount = 0;
+    // for each position on the given board
+    for(var y = 0; y < n; y++){
+      for(var x = 0; x < n; x++){
+        // if position is empty
+        if(!board.get(y)[x]){
+          // make a new copy of the given board
+          var newBoard = new Board(board);
+          // add a piece at position to the new board
+          newBoard.set(y)[x] = 1;
+          // if there are no conflicts in the new board
+          if(!newBoard.hasAnyRooksConflicts()){
+            // if move is equal or grater than n
+            if(move >= n){
+              solutionCount++;
+            } else {
+              solutionCount += f(move + 1, board);
+            }
+          }
+          // (implied) if there arre conflicts, end this branch of the decision tree
+        }
+      }
+    }
+    return solutionCount;
+  };
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
